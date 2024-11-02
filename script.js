f7868a0dec3078a24e6fb257fbb1aaccbf8ad019
Carlos1986New <118672372+Carlos1986New@users.noreply.github.com>
2024-11-02 09:38:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-const form =  document.querySelector('form');
-const input =  document.querySelector('.input');
+const form = document.querySelector('form');
+const input = document.querySelector('.input');
 
+const isValidImageUrl = (url) => {
+    // Verifica se a URL termina com uma extensão de imagem
+    return /\.(jpeg|jpg|gif|png|bmp|svg)$/.test(url);
+};
 
 const replaceImages = (url) => {
     const images = document.querySelectorAll('img');
-
     images.forEach((image) => image.src = url);
-}
+};
 
-form.addEventListener('submit', async (event) => {    
+form.addEventListener('submit', async (event) => {
     event.preventDefault();
+    const imageUrl = input.value;
 
-    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true});
+    // Verifica se o URL fornecido é uma URL de imagem válida
+    if (!isValidImageUrl(imageUrl)) {
+        alert('Por favor, insira um link de imagem válido (JPEG, PNG, GIF, etc.).');
+        return; // Sai da função se o URL não for válido
+    }
+
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
     chrome.scripting.executeScript({
-        target: { tabId: tab.id},
+        target: { tabId: tab.id },
         function: replaceImages,
-        args: [input.value]
+        args: [imageUrl]
     });
 });
